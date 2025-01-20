aac23a90924a2e8a59082c7e06167391fab665fa
Ian <115850010+NaiZenay@users.noreply.github.com>
2025-01-20 02:10:44 -0600

    feat(nav):navegacion al componente de lanzamientos

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,8 +1,21 @@
+import { useNavigate } from "react-router-dom"
 import { List } from "react-bootstrap-icons"
 import "./nav.css"
 import { Link } from "react-router-dom"
 import grLogo from "../../assets/logos/grLogo.png"
 const Nav = () => {
+    const navigate = useNavigate();
+
+    const goToLanzamientos = () => {
+        navigate("/"); // Navega a Home
+        setTimeout(() => {
+            const section = document.getElementById("lanzamientos");
+            if (section) {
+                section.scrollIntoView({ behavior: "smooth" });
+            }
+        }, 100); // Espera a que se renderice Home
+    };
+
     return (
         <header>
             <div className="container-fluid w-100 limon-viejo-BG p-4 ">
@@ -10,7 +23,7 @@ const Nav = () => {
                     {/* Brand / Logo */}
                     <div className="col-6 col-md-2 col-lg-4">
                         <Link to="/" className="text-decoration-none text-dark fs-4 fw-bold">
-                        <img src={grLogo} alt="Grupo Requiez" className="img-fluid" />
+                            <img src={grLogo} alt="Grupo Requiez" className="img-fluid" />
                         </Link>
                     </div>
                     {/* Toggle Button */}
@@ -23,7 +36,7 @@ const Nav = () => {
                     <div className="col-lg-8 col-md-10 collapse d-md-flex justify-content-end" id="navbarMenu">
                         <ul className="nav flex-column flex-md-row align-items-center">
                             <li className="nav-item">
-                                <Link to={"/"} className="nav-link link-NAV SANS-REGULAR text-dark">LANZAMIENTOS</Link>
+                                <button onClick={goToLanzamientos} className="nav-link link-NAV SANS-REGULAR text-dark">LANZAMIENTOS</button>
                             </li>
                             <li className="nav-item dropdown">
                                 <Link to={"/marca"} className="nav-link dropdown-toggle link-NAV SANS-REGULAR text-dark" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -42,7 +55,7 @@ const Nav = () => {
 
                             <li className="nav-item dropdown">
                                 <Link to={"/marca"} className="nav-link dropdown-toggle link-NAV SANS-REGULAR text-dark" id="descargas" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                DESCARGAS
+                                    DESCARGAS
                                 </Link>
                                 <ul className="dropdown-menu border-dark rounded-3 p-3 limon-viejo-BG SANS-REGULAR" aria-labelledby="descargas">
                                     <li className="dropdown-item link-NAV p-0 limon-viejo-BG" >Descarga 1</li>
